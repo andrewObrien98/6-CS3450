e7f75f9c7efd1bfd70fbd59b69aadd8c3c2824ea
andrewObrien98 <andrewpatrickpeterson@gmail.com>
2022-02-24 11:49:32 -0700

    basic urls and views

--- app/cs3450/moneyLawndering/urls.py
+++ app/cs3450/moneyLawndering/urls.py
@@ -4,4 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signin/', views.signin, name='signin'),
+    path('signup/', views.signup, name='signup'),
+    path('<int:user_id>/account/', views.account, name='account'),
+    path('publiclistings/', views.publicListing, name='publicListing'),
+    path('<int:user_id>/mylistings/', views.myListing, name='myListing'),
+    path('newListing/', views.newListing, name='newListing'),
+    path('<int:user_id>/acceptedjobs/', views.acceptedJobs, name='acceptedJobs'),
+    path('<int:user_id>/directtransfer/', views.directTransfer, name='directTransfer'),
+    path('<int:user_id>/history/', views.history, name='history'),
+
 ]
