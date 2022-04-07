2a93d63109160f601e07c51dfe08e67c11fe3bd4
XanderMatheson <mathesonal@gmail.com>
2022-04-07 09:23:10 -0600

    Updated acceptedJobs.js to have carousel effect

--- app/cs3450/moneyLawndering/static/moneyLawndering/acceptedJobs.js
+++ app/cs3450/moneyLawndering/static/moneyLawndering/acceptedJobs.js
@@ -1,17 +1,3 @@
-const selector = document.getElementById('status')
-
-function hideAll() {
-    const possibleValues = ['appliedFor', 'accepted', 'completed'];
-    for (let i = 0; i < 3; i++) {
-        document.getElementById(possibleValues[i]).hidden = true;
-    };
-}
-
-hideAll()
-// Set selector value to match initial section being shown
-selector.value = 'accepted';
-document.getElementById('accepted').hidden = false;
-
 const selector = document.getElementById('status');
 selector.addEventListener('change', (event) => {
     const possibleValues = new Array('appliedFor', 'accepted', 'completed');
